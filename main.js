881f57bce21cd2a464994864fee3ad9ffd883d23
RandomUser-Spec <76138622+RandomUser-Spec@users.noreply.github.com>
2021-07-18 20:58:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preLoad()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(400, 400);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('filtered_image.png')
+}
